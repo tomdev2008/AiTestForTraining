faa307497304ee4ad4dd680a6350e3a6019f8bbc
Wayne Yang <wayel_yang@qq.com>
2018-02-22 14:37:24 +0800

    add pic

--- AiTestForTraining/Properties/Resources.Designer.cs
+++ AiTestForTraining/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace AiTestForTraining.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap car {
+            get {
+                object obj = ResourceManager.GetObject("car", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap topic {
+            get {
+                object obj = ResourceManager.GetObject("topic", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap 微信图片_20180221163013 {
             get {
                 object obj = ResourceManager.GetObject("微信图片_20180221163013", resourceCulture);
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # AiTestForTraining
 （脱敏，不涉密）用于《途虎与AI》内部培训的测试案例
 
+下载后直接运行 /exc/AiTestForTraining.exe 即可。
+
+源码：/AiTestForTraining
+
 基础SDK采用百度AIP：https://github.com/Baidu-AIP
 
 ![预览1](https://github.com/unilog/AiTestForTraining/blob/master/AiTestForTraining/Resources/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20180221171916.png)
